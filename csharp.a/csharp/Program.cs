9810e9383a6b3873073aba569e29291fc32bfbd7
August Feng <augustfengd@gmail.com>
2023-02-21 11:27:33 -0500

    add csharp using fsharp code

---
+++ csharp.a/csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.FSharp.Core;
+using Foo;
+
+namespace csharp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var m = Bar.construct();
+        var func = m.Increment;
+        func.Invoke("a").Invoke("b"); // returns a function because currying, so nothing printed.
+        func.Invoke("a").Invoke("b").Invoke("c") // currying has finished, print function will be invoked;
+    }
+}
---
+++ csharp.a/fsharp/Program.fs
@@ -0,0 +1,11 @@
+﻿namespace Foo
+
+module Bar =
+    type Metrics =
+        { Increment : string -> string -> string -> unit }
+        interface System.IDisposable with
+            member this.Dispose() = ()
+
+    let construct () =
+        { Increment = fun a b c -> printfn "%s %s %s" a b c }
+
